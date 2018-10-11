96681c92b6bd652a855d4a6646bda6f7c7e5e0d0
SightVision <jhel@avans.nl>
2018-10-11 14:38:10 +0200

    add DateToString in View.java

--- src/view/View.java
+++ src/view/View.java
@@ -16,24 +16,6 @@ public abstract class View extends JPanel {
         return this.viewName;
     }
 
-    public void setTextFieldInvalid(JTextField field) {
-        field.setBorder(BorderFactory.createDashedBorder(Color.RED));
-    }
-
-    public void setTextFieldValid(JTextField field) {
-        field.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-    }
-
-    public void resetErrorLabel(JLabel label) {
-        label.setText("");
-        label.setVisible(false);
-    }
-
-    public void showErrorLabel(JLabel label, String text) {
-        label.setText(text);
-        label.setVisible(true);
-    }
-
     public String convertDateToString(Date dateObject){
         Date date = dateObject;
         DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
